4248a5f426bb69d59dd879ec61dd6f1e614caf9e
Jorge Totena <jorge.totena@gmail.com>
2022-02-08 14:56:01 -0500

    Eliminando pacientes con Event Bubbling

--- JAVASCRIPT/Proyecto-curso/js/eliminar-paciente.js
+++ JAVASCRIPT/Proyecto-curso/js/eliminar-paciente.js
@@ -1,8 +1,16 @@
 var pacientes = document.querySelectorAll(".paciente")
-console.log(pacientes);
+var tabla = document.querySelector("#tabla-pacientes")
 
+tabla.addEventListener("dblclick",function(event){
+    console.log("hicieron clicks");  //cuando hace click dentro de la tabla manda el console.log, se resuelve le problema de la tabla nativa
+    event.target.parentNode.remove() //lugar donde estoy haciendo especificamente el click
+
+});
+
+/*
 pacientes.forEach(function(paciente) { //for each es más sencillo de usar para esta aplicación, esta función solo sirve para lo nativo
     paciente.addEventListener("dblclick", function(){
         this.remove(); //me remueve el objeto que estoy seleccionando        
     });  
-});
+}); 
+*/
