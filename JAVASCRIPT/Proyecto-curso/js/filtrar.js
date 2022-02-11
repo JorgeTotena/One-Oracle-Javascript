f091eb352744102858910391c3a21b9a36538eee
Jorge Totena <jorge.totena@gmail.com>
2022-02-11 07:23:31 -0500

    Agregando inteligencia al filtro para que no muestre los otros datos, borre y cambiando estilos

--- JAVASCRIPT/Proyecto-curso/js/filtrar.js
+++ JAVASCRIPT/Proyecto-curso/js/filtrar.js
@@ -1,18 +1,30 @@
 var campoFiltro = document.querySelector("#filtrar-tabla")
 console.log(campoFiltro);
 
-campoFiltro.addEventListener("input",function(){
-    console.log("Escribieron en el Input");
-    console.log(this.value); 
-
+campoFiltro.addEventListener("input",function(){ //creando filtro
     var pacientes = document.querySelectorAll(".paciente")
-    console.log(pacientes); 
+    
+    
+    if(this.value.length > 0){ 
 
-    for(var i = 0; i < pacientes.length; i++){
-        var paciente = pacientes[i]
-        var tdNombre = paciente.querySelector(".info-nombre");
-        var nombre = tdNombre.textContent;
-        console.log(nombre);
+        for(var i = 0; i < pacientes.length; i++){ //haciendo la iteracción
+            var paciente = pacientes[i]
+            var tdNombre = paciente.querySelector(".info-nombre");
+            var nombre = tdNombre.textContent;
+        
+            if(nombre != this.value){ // haciendo la comparación del filtro
+                paciente.classList.add("invisible") // mirar css
+
+            }else{
+                paciente.classList.remove("invisible")
+            }
+        }
+    }else{
+        for(var i = 0; i < pacientes.length; i++){ //haciendo la iteracción, para que cuando borre me vuelva a mostrar la tabla
+            var paciente = pacientes[i]
+            paciente.classList.remove("invisible");
+        }
     }
 
 });
+
