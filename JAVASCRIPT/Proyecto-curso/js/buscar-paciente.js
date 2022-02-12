1ce0884286a6718532833555c92aa08e26e5ce0c
Jorge Totena <jorge.totena@gmail.com>
2022-02-12 12:41:40 -0500

    Conectando con un API AJAX

--- JAVASCRIPT/Proyecto-curso/js/buscar-paciente.js
+++ JAVASCRIPT/Proyecto-curso/js/buscar-paciente.js
@@ -2,5 +2,12 @@ var botonBuscar = document.querySelector("#buscar-paciente");
 
 botonBuscar.addEventListener("click", function(){
     console.log("Buscando pacientes");
+    var xhr = new XMLHttpRequest; //permite intercambiar datos entre la web y servidores, permite actualizar paginas sin recargarlas
+    xhr.open("GET", "https://alura-es-cursos.github.io/api-pacientes/pacientes.json")
+    xhr.addEventListener("load", function(){
+        console.log(xhr.responseText);
 
+
+    }); //este evento es para la requisciión del api, conecta ambos
+    xhr.send()
 });
